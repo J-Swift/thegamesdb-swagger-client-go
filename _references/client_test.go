06967c20ca1d18e7fbba5929d1b4bf930052956d
Jimmy Reichley <jimmyqpublik@gmail.com>
2022-05-20 21:35:33 -0400

    Generate with new openapi generator

--- _references/client_test.go
+++ _references/client_test.go
@@ -33,59 +33,59 @@ func TestApiClient(t *testing.T) {
 
 	t.Log("Checking GamesAPI...")
 
-	_, resp, err := client.GamesApi.GamesByGameID(ctx, apikey, "1", nil)
+	_, resp, err := client.GamesApi.GamesByGameID(ctx).Apikey(apikey).Id("1").Execute()
 	apicalls = append(apicalls, apiCallResult{endpoint: "GamesByGameID", resp: resp, err: err})
 
-	_, resp, err = client.GamesApi.GamesByGameName(ctx, apikey, "halo", nil)
+	_, resp, err = client.GamesApi.GamesByGameName(ctx).Apikey(apikey).Name("halo").Execute()
 	apicalls = append(apicalls, apiCallResult{endpoint: "GamesByGameName", resp: resp, err: err})
 
-	_, resp, err = client.GamesApi.GamesByGameNameV1(ctx, apikey, "halo", nil)
+	_, resp, err = client.GamesApi.GamesByGameNameV1(ctx).Apikey(apikey).Name("halo").Execute()
 	apicalls = append(apicalls, apiCallResult{endpoint: "GamesByGameName_v1", resp: resp, err: err})
 
-	_, resp, err = client.GamesApi.GamesByPlatformID(ctx, apikey, "1", nil)
+	_, resp, err = client.GamesApi.GamesByPlatformID(ctx).Apikey(apikey).Id("1").Execute()
 	apicalls = append(apicalls, apiCallResult{endpoint: "GamesByPlatformID", resp: resp, err: err})
 
-	_, resp, err = client.GamesApi.GamesImages(ctx, apikey, "1", nil)
+	_, resp, err = client.GamesApi.GamesImages(ctx).GamesId("1").Apikey(apikey).Execute()
 	apicalls = append(apicalls, apiCallResult{endpoint: "GamesImages", resp: resp, err: err})
 
-	_, resp, err = client.GamesApi.GamesUpdates(ctx, apikey, 0, nil)
+	_, resp, err = client.GamesApi.GamesUpdates(ctx).Apikey(apikey).LastEditId(0).Execute()
 	apicalls = append(apicalls, apiCallResult{endpoint: "GamesUpdates", resp: resp, err: err})
 
 	// DevelopersApi
 
 	t.Log("Checking DevelopersApi...")
 
-	_, resp, err = client.DevelopersApi.Developers(ctx, apikey)
+	_, resp, err = client.DevelopersApi.Developers(ctx).Apikey(apikey).Execute()
 	apicalls = append(apicalls, apiCallResult{endpoint: "Developers", resp: resp, err: err})
 
 	// GenresApi
 
 	t.Log("Checking GenresApi...")
 
-	_, resp, err = client.GenresApi.Genres(ctx, apikey)
+	_, resp, err = client.GenresApi.Genres(ctx).Apikey(apikey).Execute()
 	apicalls = append(apicalls, apiCallResult{endpoint: "Genres", resp: resp, err: err})
 
 	// PlatformsApi
 
 	t.Log("Checking PlatformsApi...")
 
-	_, resp, err = client.PlatformsApi.Platforms(ctx, apikey, nil)
+	_, resp, err = client.PlatformsApi.Platforms(ctx).Apikey(apikey).Execute()
 	apicalls = append(apicalls, apiCallResult{endpoint: "Platforms", resp: resp, err: err})
 
-	_, resp, err = client.PlatformsApi.PlatformsByPlatformID(ctx, apikey, 1, nil)
+	_, resp, err = client.PlatformsApi.PlatformsByPlatformID(ctx).Apikey(apikey).Id(1).Execute()
 	apicalls = append(apicalls, apiCallResult{endpoint: "PlatformsByPlatformID", resp: resp, err: err})
 
-	_, resp, err = client.PlatformsApi.PlatformsByPlatformName(ctx, apikey, "3do", nil)
+	_, resp, err = client.PlatformsApi.PlatformsByPlatformName(ctx).Apikey(apikey).Name("3do").Execute()
 	apicalls = append(apicalls, apiCallResult{endpoint: "PlatformsByPlatformName", resp: resp, err: err})
 
-	_, resp, err = client.PlatformsApi.PlatformsImages(ctx, apikey, "1", nil)
+	_, resp, err = client.PlatformsApi.PlatformsImages(ctx).Apikey(apikey).PlatformsId("1").Execute()
 	apicalls = append(apicalls, apiCallResult{endpoint: "PlatformsImages", resp: resp, err: err})
 
 	// PublishersApi
 
 	t.Log("Checking PublishersApi...")
 
-	_, resp, err = client.PublishersApi.Publishers(ctx, apikey)
+	_, resp, err = client.PublishersApi.Publishers(ctx).Apikey(apikey).Execute()
 	apicalls = append(apicalls, apiCallResult{endpoint: "Publishers", resp: resp, err: err})
 
 	for _, apiCall := range apicalls {
